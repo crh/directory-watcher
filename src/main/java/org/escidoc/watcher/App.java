1dded4e7cda6ae596aafe0ac8aef8d0e1952f5ec
Christian Herlambang <christian.herlambang@gmail.com>
2011-04-13 17:00:17 +0200

    set compiler setting to 1.7, organize imports

--- src/main/java/org/escidoc/watcher/App.java
+++ src/main/java/org/escidoc/watcher/App.java
@@ -21,65 +21,62 @@ public class App {
     private static Path path;
 
     public static void main(final String... args) throws Exception {
-        if (args.length == 0) {
-            useDefaultPath();
-        }
-        else if (args.length > 1) {
-            LOG.error("One by one, Master...");
-        }
-        else {
-            path = Paths.get(args[0]);
-            final WatchService service = init();
-            for (;;) {
-                usingPull(service);
-            }
-        }
+	if (args.length == 0) {
+	    useDefaultPath();
+	} else if (args.length > 1) {
+	    LOG.error("One by one, Master...");
+	} else {
+	    path = Paths.get(args[0]);
+	    final WatchService service = init();
+	    for (;;) {
+		usingPull(service);
+	    }
+	}
 
     }
 
     private static void useDefaultPath() {
-        path = Paths.get(TO_MONITOR);
+	path = Paths.get(TO_MONITOR);
     }
 
     private static WatchService init() throws IOException {
-        final WatchService service = FileSystems.getDefault().newWatchService();
-        path.register(service, StandardWatchEventKind.ENTRY_CREATE,
-            StandardWatchEventKind.ENTRY_DELETE,
-            StandardWatchEventKind.ENTRY_MODIFY,
-            StandardWatchEventKind.OVERFLOW);
-        return service;
+	final WatchService service = FileSystems.getDefault().newWatchService();
+	path.register(service, StandardWatchEventKind.ENTRY_CREATE,
+		StandardWatchEventKind.ENTRY_DELETE,
+		StandardWatchEventKind.ENTRY_MODIFY,
+		StandardWatchEventKind.OVERFLOW);
+	return service;
     }
 
     private static void usingPull(final WatchService service)
-        throws InterruptedException {
-        extract(service.take());
+	    throws InterruptedException {
+	extract(service.take());
     }
 
     private static void extract(final WatchKey key) {
-        checkForValidity(key);
-        key.reset();
+	checkForValidity(key);
+	key.reset();
     }
 
     private static void checkForValidity(final WatchKey key) {
-        if (isInvalid(key)) {
-            LOG.warn("Invalid key: " + key);
-        }
-        else {
-            processEvent(key);
-        }
+	if (isInvalid(key)) {
+	    LOG.warn("Invalid key: " + key);
+	} else {
+	    processEvent(key);
+	}
     }
 
     private static void processEvent(final WatchKey key) {
-        for (final WatchEvent<?> watchEvent : key.pollEvents()) {
-            final String name = watchEvent.kind().name();
-            final Class<?> type = watchEvent.kind().type();
-            final Path context = (Path) watchEvent.context();
-            final Path resolvedPath = path.resolve(context);
-            LOG.info("event kind: " + name + " context: " + resolvedPath);
-        }
+	for (final WatchEvent<?> watchEvent : key.pollEvents()) {
+	    final String name = watchEvent.kind().name();
+	    final Class<?> type = watchEvent.kind().type();
+	    final Path context = (Path) watchEvent.context();
+	    final Path resolvedPath = path.resolve(context);
+	    LOG.info("event kind: " + name + " context: " + resolvedPath);
+	}
     }
 
     private static boolean isInvalid(final WatchKey key) {
-        return !key.isValid();
+	return !key.isValid();
     }
 }
